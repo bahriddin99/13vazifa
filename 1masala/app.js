7133686947566bb1a36360456245be1841a43571
Bahriddin <Bahriddin>
2023-11-02 23:59:47 +0500

    yakunlandi

---
+++ 1masala/app.js
@@ -0,0 +1,17 @@
+let image = [
+  "https://cs9.pikabu.ru/post_img/2017/10/06/0/1507239211153216060.jpg",
+  "https://evg-crystal.ru/800/600/https/static3.depositphotos.com//1003388/172/i/450/depositphotos_1720581-stock-photo-3d-render-of-light-bulb.jpg",
+];
+
+let imagElem = document.querySelector("img");
+
+let btnElem = document.querySelector("#btn");
+
+let i = 1;
+btnElem.addEventListener("click", () => {
+  imagElem.setAttribute("src", image[i]);
+  i++;
+  if (i === 2) {
+    i = 0;
+  }
+});
